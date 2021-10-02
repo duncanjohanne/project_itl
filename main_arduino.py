1500e6663e80b7588c8a201a88e053e439c1b192
duncanjohanne <duncankachasu@gmail.com>
2021-10-02 08:57:37 +0200

    fintouch

--- main_arduino.py
+++ main_arduino.py
@@ -15,7 +15,7 @@ led_red_ = 10
 led_amber_ = 9
 led_green_ = 8
 port = 'COM12'
-board = 0 #pyfirmata.Arduino(port)
+board = pyfirmata.Arduino(port)
 
 index = 0
 
@@ -37,25 +37,28 @@ while True:
     displaycount = display_data(delayA, delayB, countAB, countC, displaycount)
     logTrafficStats(file, countC, countAB, delayB, delayA, boon)
 
-    # board.digital['led_green'].write(1)
-    # time.sleep(round(delayA/10, 1))
-    # board.digital['led_green'].write(0) 
-    # board.digital['led_amber'].write(1)
-    # time.sleep(2)
-    # board.digital['led_amber'].write(0) 
-    # board.digital['led_red'].write(1)
-    # time.sleep(1)
+    board.digital[led_red_].write(1)
+    board.digital[led_green].write(1)
+    time.sleep(round(delayA/10, 1))
+    board.digital[led_green].write(0) 
+    board.digital[led_amber].write(1)
+    time.sleep(2)
+    board.digital[led_amber].write(0) 
+    board.digital[led_red].write(1)
+    time.sleep(1)
 
-    # board.digital['led_red_'].write(0)
-    # time.sleep(1)
-    # board.digital['led_green_'].write(1)
-    # time.sleep(round(delayB/10, 1))
-    # board.digital['led_green_'].write(0)
-    # board.digital['led_amber_'].write(1)
-    # time.sleep(2)
-    # board.digital['led_amber_'].write(0) 
-    # board.digital['led_red_'].write(1)
-    # time.sleep(1) 
+    board.digital[led_red_].write(0)
+    time.sleep(1)
+    board.digital[led_green_].write(1)
+    time.sleep(round(delayB/10, 1))
+    board.digital[led_green_].write(0)
+    board.digital[led_amber_].write(1)
+    time.sleep(2)
+    board.digital[led_amber_].write(0) 
+    board.digital[led_red_].write(1)
+    time.sleep(0.5)
+    board.digital[led_red].write(0)
+    time.sleep(1) 
 
     index = index + 1
     if index == 12:
